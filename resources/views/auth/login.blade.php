b627f40691986acd35a492faab149f9f08d6ddf0
NikarashiHatsu <yourlovelydev@gmail.com>
2022-06-29 15:26:25 +0700

    chore: styling auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,80 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+    <form class="card card-md" action="{{ route('login') }}" method="POST" autocomplete="off">
+        @csrf
+        <div class="card-body">
+            <h2 class="card-title text-center mb-4">Masuk menggunakan akun Anda</h2>
+            <div class="mb-3">
+                <label class="form-label">Alamat Email</label>
+                <input
+                    @class(['form-control', 'is-invalid' => $errors->has('email')])
+                    type="email"
+                    name="email"
+                    class="form-control"
+                    placeholder="Enter email"
+                />
+                @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+            <div class="mb-2">
+                <label class="form-label">
+                    Kata Sandi
+                </label>
+                <div
+                    @class(['input-group input-group-flat', 'is-invalid' => $errors->has('password')])
+                    x-data="{ showPassword: false }"
+                >
+                    <input
+                        @class(['form-control', 'is-invalid' => $errors->has('password')])
+                        x-bind:type="showPassword ? 'text' : 'password'"
+                        type="password"
+                        placeholder="Password"
+                        autocomplete="off"
+                        name="password"
+                    />
+                    <span class="input-group-text">
+                        <a
+                            x-on:click="showPassword = !showPassword"
+                            href="javascript:void(0)"
+                            class="link-secondary"
+                            title="Show password"
+                            data-bs-toggle="tooltip"
+                        >
+                            <svg x-show="!showPassword" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <circle cx="12" cy="12" r="2"></circle>
+                                <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
+                            </svg>
+                            <svg x-show="showPassword" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <line x1="3" y1="3" x2="21" y2="21"></line>
+                                <path d="M10.584 10.587a2 2 0 0 0 2.828 2.83"></path>
+                                <path d="M9.363 5.365a9.466 9.466 0 0 1 2.637 -.365c4 0 7.333 2.333 10 7c-.778 1.361 -1.612 2.524 -2.503 3.488m-2.14 1.861c-1.631 1.1 -3.415 1.651 -5.357 1.651c-4 0 -7.333 -2.333 -10 -7c1.369 -2.395 2.913 -4.175 4.632 -5.341"></path>
+                            </svg>
+                        </a>
+                    </span>
+                </div>
+                @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <div class="mb-2">
+                <label class="form-check">
+                    <input type="checkbox" class="form-check-input" name="remember" />
+                    <span class="form-check-label">Ingat saya</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
+            <div class="form-footer">
+                <button type="submit" class="btn btn-primary w-100">Masuk</button>
             </div>
-        </form>
-    </x-auth-card>
+        </div>
+    </form>
+    <div class="text-center text-muted mt-3">
+        <span>
+            Belum punya akun?
+        </span>
+        <a href="{{ route('register') }}" tabindex="-1">
+            Daftar disini
+        </a>
+    </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,21 +1,44 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta5
+* @link https://tabler.io
+* Copyright 2018-2022 The Tabler Authors
+* Copyright 2018-2022 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="{{ asset('dist/css/tabler-flags.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler-payments.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet" />
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<body class=" border-top-wide border-primary d-flex flex-column">
+    <div class="page page-center">
+        <div class="container-tight py-4">
+            <div class="text-center mb-4">
+                <a href="{{ route('index') }}" class="navbar-brand navbar-brand-autodark">
+                    <img src="{{ asset('./static/logo.svg') }}" height="36" alt="">
+                </a>
+            </div>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
-    </body>
+    </div>
+
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="{{ asset('dist/js/tabler.min.js') }}"></script>
+</body>
+
 </html>
